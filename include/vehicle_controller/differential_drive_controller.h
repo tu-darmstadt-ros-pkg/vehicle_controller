b7366fb2b94ababb2a236d86aa27679211102067
Paul Manns <pfmanns@gmail.com>
2016-01-05 07:37:52 +0100

    changed comment

--- include/vehicle_controller/differential_drive_controller.h
+++ include/vehicle_controller/differential_drive_controller.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2014, Stefan Kohlbrecher
+    Copyright (c) 2015, Paul Manns, Stefan Kohlbrecher
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
